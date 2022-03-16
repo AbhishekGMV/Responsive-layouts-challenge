ef921e768e754fdf927b5d6fefe10c256faf5ae8
Abhishek Gowda M V <abhishek.mvg@gmail.com>
2022-03-16 10:08:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav-toggle");
+let navList = document.querySelector(".nav__list");
 
 navToggle.addEventListener("click", () => {
   nav.classList.toggle("open");
+  navList.style.display 
+    = navList.style.display === "block" ? "none" : "block";
 });
